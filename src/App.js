700046c3334a801c687722c124e9e3775d470080
Aditya Alshi <alshiaditya55@gmail.com>
2024-03-07 09:39:47 +0530

    category filter added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
+
 export default function App() {
+  const [products, setProducts] = React.useState([]);
+  const [filteredProducts, setFilteredProducts] = React.useState("all");
+
+  function handleCategoryClick(event) {
+    const productname = event.target.getAttribute("name");
+    console.log(productname);
+    setFilteredProducts(productname);
+  }
+  const finalProducts = products.filter(
+    (product) => product.category === filteredProducts
+  );
+
+  React.useEffect(() => {
+    const callfecth = async () => {
+      try {
+        const res = await fetch(
+          "https://dummyjson.com/products?limit=100&select=title,price,brand,category,thumbnail,images,discountPercentage"
+        );
+
+        const jsonConverted = await res.json();
+        const data = jsonConverted.products;
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    callfecth();
+  }, []);
+
   return (
     <div className="app--wrapper">
       <header className="header--wrapper">
@@ -25,30 +58,159 @@ export default function App() {
         <aside className="categories--wrapper">
           <p className="categories--title">Categoies</p>
           <ul className="categories--list">
-            <li className="categories--list--items item--1">smartphones</li>
-            <li className="categories--list--items item--2">laptops</li>
-            <li className="categories--list--items item--3">fragrances</li>
-            <li className="categories--list--items item--4">skincare</li>
-            <li className="categories--list--items item--5">groceries</li>
-            <li className="categories--list--items item--6">home-decoration</li>
-            <li className="categories--list--items item--7">furniture</li>
-            <li className="categories--list--items item--8">tops</li>
-            <li className="categories--list--items item--9">womens-dresses</li>
-            <li className="categories--list--items item--10">womens-shoes</li>
-            <li className="categories--list--items item--11">mens-shirts</li>
-            <li className="categories--list--items item--12">mens-shoes</li>
-            <li className="categories--list--items item--13">mens-watches</li>
-            <li className="categories--list--items item--14">womens-watches</li>
-            <li className="categories--list--items item--15">womens-bags</li>
-            <li className="categories--list--items item--16">womens-jewellery</li>
-            <li className="categories--list--items item--17">sunglasses</li>
-            <li className="categories--list--items item--18">automotive</li>
-            <li className="categories--list--items item--19">motorcycle</li>
-            <li className="categories--list--items item--20">lighting</li>
+            <li
+              className="categories--list--items item--1"
+              name="all"
+              onClick={handleCategoryClick}
+            >
+              allProducts
+            </li>
+            <li
+              className="categories--list--items item--1"
+              name="smartphones"
+              onClick={handleCategoryClick}
+            >
+              smartphones
+            </li>
+            <li
+              className="categories--list--items item--2"
+              name="laptops"
+              onClick={handleCategoryClick}
+            >
+              laptops
+            </li>
+            <li
+              className="categories--list--items item--3"
+              name="fragrances"
+              onClick={handleCategoryClick}
+            >
+              fragrances
+            </li>
+            <li
+              className="categories--list--items item--4"
+              name="skincare"
+              onClick={handleCategoryClick}
+            >
+              skincare
+            </li>
+            <li
+              className="categories--list--items item--5"
+              name="groceries"
+              onClick={handleCategoryClick}
+            >
+              groceries
+            </li>
+            <li
+              className="categories--list--items item--6"
+              name="home-decoration"
+              onClick={handleCategoryClick}
+            >
+              home-decoration
+            </li>
+            <li
+              className="categories--list--items item--7"
+              name="furniture"
+              onClick={handleCategoryClick}
+            >
+              furniture
+            </li>
+            <li
+              className="categories--list--items item--8"
+              name="tops"
+              onClick={handleCategoryClick}
+            >
+              tops
+            </li>
+            <li
+              className="categories--list--items item--9"
+              name="womens-dresses"
+              onClick={handleCategoryClick}
+            >
+              womens-dresses
+            </li>
+            <li
+              className="categories--list--items item--10"
+              name="womens-shoes"
+              onClick={handleCategoryClick}
+            >
+              womens-shoes
+            </li>
+            <li
+              className="categories--list--items item--11"
+              name="mens-shirts"
+              onClick={handleCategoryClick}
+            >
+              mens-shirts
+            </li>
+            <li
+              className="categories--list--items item--12"
+              name="mens-shoes"
+              onClick={handleCategoryClick}
+            >
+              mens-shoes
+            </li>
+            <li
+              className="categories--list--items item--13"
+              name="mens-watches"
+              onClick={handleCategoryClick}
+            >
+              mens-watches
+            </li>
+            <li
+              className="categories--list--items item--14"
+              name="womens-watches"
+              onClick={handleCategoryClick}
+            >
+              womens-watches
+            </li>
+            <li
+              className="categories--list--items item--15"
+              name="womens-bags"
+              onClick={handleCategoryClick}
+            >
+              womens-bags
+            </li>
+            <li
+              className="categories--list--items item--16"
+              name="womens-jewellery"
+              onClick={handleCategoryClick}
+            >
+              womens-jewellery
+            </li>
+            <li
+              className="categories--list--items item--17"
+              name="sunglasses"
+              onClick={handleCategoryClick}
+            >
+              sunglasses
+            </li>
+            <li
+              className="categories--list--items item--18"
+              name="automotive"
+              onClick={handleCategoryClick}
+            >
+              automotive
+            </li>
+            <li
+              className="categories--list--items item--19"
+              name="motorcycle"
+              onClick={handleCategoryClick}
+            >
+              motorcycle
+            </li>
+            <li
+              className="categories--list--items item--20"
+              name="lighting"
+              onClick={handleCategoryClick}
+            >
+              lighting
+            </li>
           </ul>
         </aside>
         <div className="product--section">
-          <Outlet />
+          <Outlet
+            context={filteredProducts === "all" ? products : finalProducts}
+          />
         </div>
       </main>
     </div>
--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -1,43 +1,40 @@
 import React from "react";
 import { IoCartOutline } from "react-icons/io5";
+import { useOutletContext } from "react-router-dom";
 
 export default function AllProducts() {
-  const [products, setProducts] = React.useState([]);
-
-  React.useEffect(() => {
-    const callfecth = async () => {
-      try {
-        const res = await fetch(
-          "https://dummyjson.com/products?limit=100&select=title,price,brand,category,thumbnail,images,discountPercentage"
-        );
-
-        const jsonConverted = await res.json();
-        const data = jsonConverted.products;
-        console.log(data);
-        setProducts(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    callfecth();
-  }, []);
+  
+  const products = useOutletContext();
 
   const renderProducts = products.map((product) => (
     <div key={product.id} className="product--item--wrapper">
-        <div className="product--image--wrapper">
-            <img
-                className="product--item--image"
-                src={product.thumbnail}
-                alt="product"
-            />
-        </div>
+      <div className="product--image--wrapper">
+        <img
+          className="product--item--image"
+          src={product.thumbnail}
+          alt="product"
+        />
+      </div>
       <div className="product--item--details">
-        <p className="product--detail--i product--category">{product.category}</p>
+        <p className="product--detail--i product--category">
+          {product.category}
+        </p>
         <p className="product--detail--i product--title">{product.title}</p>
-        <p className="product--detail--i product--price">${(product.price - (product.price*(product.discountPercentage/100))).toFixed(2)}</p>
-        <p className="product--detail--i product--discount--price"><s>{product.price}</s><span>&nbsp; &nbsp;-{product.discountPercentage}%</span></p>
-        <button className="product--detail--i" type="button"> <IoCartOutline />Add to Cart </button>
+        <p className="product--detail--i product--price">
+          $
+          {(
+            product.price -
+            product.price * (product.discountPercentage / 100)
+          ).toFixed(2)}
+        </p>
+        <p className="product--detail--i product--discount--price">
+          <s>{product.price}</s>
+          <span>&nbsp; &nbsp;-{product.discountPercentage}%</span>
+        </p>
+        <button className="product--detail--i" type="button">
+          <IoCartOutline />
+          Add to Cart
+        </button>
       </div>
     </div>
   ));
